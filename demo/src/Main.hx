90074726fa6e2c0151d054500aeb2c452aa06b3d
Jason <jraggie08@gmail.com>
2018-01-12 15:31:23 -0700

    Added sprites and updated main to try and load animation

--- demo/src/Main.hx
+++ demo/src/Main.hx
@@ -1,33 +1,56 @@
+import h2d.Anim;
+import h2d.Bitmap;
+import h2d.Sprite;
 import h2d.Text;
+import h2d.Tile;
 import hxd.Res;
+import hxd.res.Font;
 
 class Main extends hxd.App {
-        var text : Text;
+        
+		var animation:Anim;
 
 		// Called on creation
         override function init() {
-			// Initialize all loaders for embeded resources
-			Res.initEmbed();
-			// Create an instance of wireframe.tff located in our res folder, then create a font2d of 128pt size
-			var font = Res.firacode.build(128);
-			// Create a new text object using the newly created font, parented to the 2d scene
-            text = new Text(font, s2d);
-			// Assign the text
-			text.text = "Hello World";
-			// Make it read, using hex code ( RR GG BB, each two hex characters represent an RGB value from 0 - 255 )
-			text.textColor = 0xFF0000;
+			animation = new Anim(jump(), 10, s2d);
         }
 
-		// Called each frame
-        override function update(dt:Float) {
-			// simply scale our text object up until it's 3x normal size, repeat forever
-			var scaleAmount = 0.01;
-			if (text.scaleX < 3.0) text.setScale(text.scaleX + scaleAmount);
-			else text.setScale(1);
-			
-        }
+
+		static function shoot() {
+			var shootTiles = new Array<Tile>();
+			for(resource in Res.Shoot) {
+				shootTiles.push(resource.toTile());
+			}
+			return shootTiles;
+		}
+
+		static function slide() {
+			var slideTiles = new Array<Tile>();
+			for(resource in Res.Slide) {
+				slideTiles.push(resource.toTile());
+			}
+			return slideTiles;
+		}
+
+		static function jump() {
+			var jumpTiles = new Array<Tile>();
+			for(resource in Res.Jump) {
+				jumpTiles.push(resource.toTile());
+			}
+			return jumpTiles;
+		}
+
+		static function run() {
+			var runTiles = new Array<Tile>();
+			for(resource in Res.Run) {
+				runTiles.push(resource.ToTile());
+			}
+			return runTiles;
+		}
 
         static function main() {
+
+			Res.initEmbed();
             new Main();
         }
     }
